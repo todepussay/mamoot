bfa136843b308440f796f8f51dbd0a08f995b41f
Tom Depussay <todepussay@stpbb.org>
2024-01-26 16:49:15 +0100

    Page création du jeu

--- server/index.js
+++ server/index.js
@@ -22,18 +22,49 @@ const io = socketIo(server, {
     }
 });
 
-let room = [];
+let rooms = {};
 
 io.on('connection', (socket) => {
 
-    socket.on("welcome", (message) => {
-        console.log(message);
-    })
+    socket.on('disconnecting', (data) => {
+        delete rooms[data.userId];
+    });
 
-    socket.on("createdRoom", ({ id_user, quiz, questions }) => {
-        console.log(id_user, quiz, questions);
+    socket.on("createdRoom", ({ id_user, quiz }) => {
+        let code = createCode();
+        rooms[id_user] = {
+            id: socket.id,
+            quiz: quiz,
+            code: code
+        };
+        console.log("Création d'une nouvelle room");
+
+        io.to(socket.id).emit("room", { quiz, code });
     })
 
 });
+
+function createCode(){
+
+    while (true){
+        var code = '';
+        var good = true;
+        for (var i = 0; i < 6; i++) {
+            var digit = Math.floor(1 + Math.random() * 9);
+            code += digit;
+        }
+
+        for(let i = 0; i < rooms.length; i++){
+            if (rooms[i].code === code){
+                good = false;
+            }
+        }
+
+        if(good){
+            return code;
+        }
+
+    }
+}
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
