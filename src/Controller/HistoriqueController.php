1b0ab6f7882aab30259b711673bd429547e52e2c
Tom Depussay <todepussay@stpbb.org>
2024-02-07 15:37:33 +0100

    Ajout historique

--- src/Controller/HistoriqueController.php
+++ src/Controller/HistoriqueController.php
@@ -19,8 +19,43 @@ class HistoriqueController extends AbstractController
         $repo = $em->getRepository(QuizHistorique::class);
         $resultat = $repo->findByUser($this->getUser()->getId());
 
+        $liste = [];
+
+        for($i = 0; $i < count($resultat); $i++) {
+            $liste[$i] = [
+                "id" => $resultat[$i]->getId(),
+                "date" => $resultat[$i]->getCreatedDate(),
+                "title" => $resultat[$i]->getQuiz()->getTitle(),
+                "players" => []
+            ];
+
+            foreach($resultat[$i]->getPlayers() as $player){
+                $liste[$i]["players"][]= [
+                    "id" => $player->getId(),
+                    "username" => $player->getUsername(),
+                    "score" => $player->getScore()
+                ];
+            }
+        }
+
         return $this->render('historique/index.html.twig', [
-            "activeTab" => "historique"
+            "activeTab" => "historique",
+            "liste" => $liste
         ]);
+    }
+
+    #[Route("/delete/{id}", name: "delete-historique")]
+    public function delete(EntityManagerInterface $em, $id){
+        $this->denyAccessUnlessGranted('ROLE_USER');
+
+        $repo = $em->getRepository(QuizHistorique::class);
+        $quiz = $repo->find($id);
+
+        if($quiz->getUser() === $this->getUser()) {
+            $em->remove($quiz);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute("historique_index");
     }
 }
