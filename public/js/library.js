eb9e52a5594374363f55ad8a5defa2c4ad91383d
spoltron <stpoltron@stpbb.org>
2024-02-09 15:28:43 +0100

    mise a jour de l'easter eggs

--- public/js/library.js
+++ public/js/library.js
@@ -1,19 +1,23 @@
 const easterEggCombination = ['b', 'e', 'a', 'u', 'j', 'o', 'i', 'n'];
-
 let userInput = [];
+let explosionInterval = null;
+let easterEggActive = false;
 
-function playSound(sound) {
+function playSound() {
     const audio = new Audio(`../img/beaujoin.mp3`);
     audio.play();
 }
 
 function createExplosion() {
+    if (!easterEggActive) return;
+
     const explosionImage = document.createElement('img');
     explosionImage.src = '../img/beaujoin.png';
     explosionImage.alt = 'Mamoot';
     explosionImage.style.width = '20%';
     explosionImage.style.height = 'auto';
-    explosionImage.classList.add('explosion'); // Ajouter la classe pour l'animation
+    explosionImage.style.position = 'absolute';
+    explosionImage.classList.add('explosion');
 
     const randomX = Math.random() * window.innerWidth;
     const randomY = Math.random() * window.innerHeight;
@@ -27,20 +31,29 @@ function createExplosion() {
     });
 }
 
+function toggleEasterEgg() {
+    easterEggActive = !easterEggActive; // Bascule l'état actif/inactif des explosions
+
+    if (easterEggActive) {
+        playSound();
+        explosionInterval = setInterval(createExplosion, 25);
+    } else {
+        clearInterval(explosionInterval);
+    }
+}
+
 function handleEasterEgg() {
     document.addEventListener('keydown', function (event) {
         userInput.push(event.key.toLowerCase());
+        userInput = userInput.slice(-easterEggCombination.length);
 
         if (userInput.join('') === easterEggCombination.join('')) {
-            playSound();
-            setInterval(createExplosion, 25);
-
-            setTimeout(() => {
-                userInput = [];
-            }, 2500);
+            toggleEasterEgg();
+            userInput = [];
         }
     });
 }
+
 
 document.addEventListener('DOMContentLoaded', function () {
     handleEasterEgg();
